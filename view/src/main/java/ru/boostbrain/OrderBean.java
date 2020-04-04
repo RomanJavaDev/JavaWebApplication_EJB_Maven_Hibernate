452f97a386f4dcf934475b347b91a71a5e2a6d99
RomanJavaDev <roman.oilman@mail.ru>
2020-04-04 22:02:07 +0300

    Commit - 1

---
+++ view/src/main/java/ru/boostbrain/OrderBean.java
@@ -0,0 +1,69 @@
+package ru.boostbrain;
+
+import javax.ejb.EJB;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+import ru.boostbrain.domain.Order;
+import ru.boostbrain.domain.Thing;
+import ru.boostbrain.ejb.OrdersManagerBean;
+import ru.boostbrain.ejb.ThingsManagerBean;
+
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.List;
+
+@Named
+@SessionScoped
+public class OrderBean implements Serializable {
+    private Order order;
+    private String name;
+    private int quantity;
+
+    @EJB
+    private OrdersManagerBean ordersManagerBean;
+    @EJB
+    private ThingsManagerBean thingsManagerBean;
+
+    public void createOrder() {
+        if (order == null) {
+            order = ordersManagerBean.createOrder();
+        }
+    }
+    public void createThing() {
+        thingsManagerBean.createThing(name, quantity);
+    }
+    public List<Thing> getThings() {
+        return thingsManagerBean.getThings();
+    }
+    public void addThing(Thing thing) {
+        if (order == null) {
+            return;
+        }
+        ordersManagerBean.addToOrder(thing.getId(), order.getId(), 1);
+    }
+    public List<Thing> getThingsInOrder() {
+        if (order == null) {
+            return Collections.emptyList();
+        }
+        return ordersManagerBean.getThingsInOrder(order.getId());
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
